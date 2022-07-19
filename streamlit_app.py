030cb2b7692cc3c01b8b54488c76a13a77b2b9d1
Abdoulaye Badji <58076640+ABDOULAYE2711@users.noreply.github.com>
2022-07-19 13:22:01 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,18 +5,15 @@ from PIL import Image
 load_css()
 
 col1, col2, col3 = st.columns(3)
-col2.image(Image.open('dp.png'))
+col2.image(Image.open('bd.png'))
 
-st.header('Chanin Nantasenamat, Ph.D.')
+st.header('ABDOULAYE BADJI')
 
-st.info('Developer Advocate, Content Creator and ex-Professor with an interest in Data Science and Bioinformatics')
+st.info('Data analyst, créateur de contenu et passionné de la data.')
 
 icon_size = 20
 
-st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
-st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
-st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
-st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+st_button('youtube', 'https://www.youtube.com/channel/UCZ_Sor5J6415zV8k3hNlk6Q', 'ABDOULAYE BADJI YouTube channel', icon_size)
+st_button('twitter', 'https://twitter.com/Badji2711', 'Follow me on Twitter', icon_size)
+st_button('linkedin', 'https://www.linkedin.com/in/abdoulaye-badji-a24697a0//', 'Follow me on LinkedIn', icon_size)
+st_button('facebook', 'https://www.facebook.com/abdoulaye.badji.779', 'Follow me on Facebook', icon_size)
